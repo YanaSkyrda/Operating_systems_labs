507bfffcea8909ba7ae9831b59b6063bb1a5f980
YanaSkyrda <yana.skirda101010@gmail.com>
2020-10-11 20:55:33 +0300

    Task 4 implemented.

---
+++ Task4(class-info-loader)/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        ClassInfoPrinter classInfo = new ClassInfoPrinter("exampleclasses.Animal");
+        classInfo.printInfo();
+        classInfo = new ClassInfoPrinter("exampleclasses.Cat");
+        classInfo.printInfo();
+    }
+}
