8d3ea6870e16679c7db688d9667d16522c7838dc
YanaSkyrda <yana.skirda101010@gmail.com>
2020-11-26 18:37:15 +0200

    Implemented Round-Robin algorithm and did a little refactor of original code.

---
+++ lab4/src/memorymanagment/Instruction.java
@@ -0,0 +1,12 @@
+package memorymanagment;
+
+public class Instruction {
+    String inst;
+    long addr;
+
+    public Instruction(String inst, long addr) {
+        this.inst = inst;
+        this.addr = addr;
+    }
+
+}
---
+++ lab4/src/memorymanagment/Page.java
@@ -0,0 +1,27 @@
+package memorymanagment;
+
+public class Page {
+    int id;
+    int physical;
+    byte R;
+    byte M;
+    int inMemTime;
+    int lastTouchTime;
+    long high;
+    long low;
+    boolean lastUsedInReplacement;
+
+    public Page(int id, int physical, byte R, byte M, int inMemTime, int lastTouchTime, long high, long low,
+                boolean lastUsedInReplacement) {
+        this.id = id;
+        this.physical = physical;
+        this.R = R;
+        this.M = M;
+        this.inMemTime = inMemTime;
+        this.lastTouchTime = lastTouchTime;
+        this.high = high;
+        this.low = low;
+        this.lastUsedInReplacement = lastUsedInReplacement;
+    }
+
+}
